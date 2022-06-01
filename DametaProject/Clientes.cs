3b07aac232f6ce47073ba3a55fa5743bc8e8843d
Matheus Finatto <matheusfinatto2@gmail.com>
2022-06-01 20:18:45 -0300

    codigo clientes

--- DametaProject/Clientes.cs
+++ DametaProject/Clientes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DametaProject
 {
@@ -17,9 +18,317 @@ namespace DametaProject
             InitializeComponent();
         }
 
-        private void btIncluir_Click(object sender, EventArgs e)
+        private void AtualizaListaDeClientes()
         {
+            this.premium_usuariosTableAdapter.Fill(this.dameta_dbDataSet.premium_usuarios);
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'dameta_dbDataSet.premium_usuarios'. Você pode movê-la ou removê-la conforme necessário.
+            this.premium_usuariosTableAdapter.Fill(this.dameta_dbDataSet.premium_usuarios);
 
         }
+
+
+
+        private void btIncluir_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            bool bIsOperationOK = true;
+
+            string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
+
+            conn = new SqlConnection(connectionString);
+
+            comm = new SqlCommand(
+                "INSERT INTO Clientes (Nome, CPF, ID_Cidade) " +
+                "VALUES (@Nome, @CPF, @ID_Cidade)", conn);
+
+            comm.Parameters.Add("@Nome", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@Nome"].Value = txNome.Text;
+
+            comm.Parameters.Add("@CPF", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@CPF"].Value = mtxCPF.Text;
+
+            comm.Parameters.Add("@ID_Cidade", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID_Cidade"].Value = Convert.ToInt32(cbCidade.SelectedValue);
+
+
+            try
+            {
+                try
+                {
+                    // Abre a conexão com o Banco de Dados
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(ex.Message,
+                        "Erro ao tentar abrir o Banco de Dados",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(ex.Message,
+                        "Erro ao tentar executar o comando SQL.",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch { }
+            finally
+            {
+                // Fecha a conexão com o Bando de Dados
+                conn.Close();
+
+                if (bIsOperationOK == true)
+                {
+                    MessageBox.Show("Cliente Cadastrado com sucesso!",
+                        "INSERT",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    AtualizaListaDeClientes();
+                    btLimpar_Click(sender, e);
+                }
+            }
+        }
+
+        private void btConsultar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            SqlDataReader reader;
+
+            string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
+
+            // Inicializa a conexão com o Banco de Dados
+            conn = new SqlConnection(connectionString);
+
+            comm = new SqlCommand(
+                "SELECT Cli.ID_Cliente, Cli.Nome, Cli.CPF, Cli.ID_Cidade, " +
+                "Cid.ID_Cidade, Cid.NomeCid, Cid.UF " +
+                "FROM Clientes AS Cli " +
+                "INNER JOIN Cidades AS Cid " +
+                "ON Cli.ID_Cliente = @ID AND Cid.ID_Cidade = Cli.ID_Cidade", conn);
+
+            comm.Parameters.Add("@ID", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID"].Value = Convert.ToInt32(txID.Text);
+
+            try
+            {
+                try
+                {
+                    // Abre a conexão com o Banco de Dados
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Erro ao tentar abrir o Banco de Dados",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+
+                try
+                {
+                    // Executa o comando SQL
+                    reader = comm.ExecuteReader();
+
+                    // Se encontrou um cliente...
+                    if (reader.Read())
+                    {
+                        txNome.Text = reader["Nome"].ToString();
+                        mtxCPF.Text = reader["CPF"].ToString();
+                        cbCidade.Text = reader["NomeCid"].ToString();
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Erro ao tentar executar o comando SQL.",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch { }
+            finally
+            {
+                // Fecha a conexão com o Bando de Dados
+                conn.Close();
+            }
+        }
+
+        private void btAlterar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            bool bIsOperationOK = true;
+
+            string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
+
+            conn = new SqlConnection(connectionString);
+
+            comm = new SqlCommand(
+                "UPDATE Clientes SET Nome=@Nome, CPF=@CPF, ID_Cidade=@ID_Cidade " +
+                "WHERE ID_Cliente = @ID_Cliente", conn);
+
+            comm.Parameters.Add("@ID_Cliente", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID_Cliente"].Value = Convert.ToInt32(txID.Text);
+
+            comm.Parameters.Add("@Nome", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@Nome"].Value = txNome.Text;
+
+            comm.Parameters.Add("@CPF", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@CPF"].Value = mtxCPF.Text;
+
+            comm.Parameters.Add("@ID_Cidade", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID_Cidade"].Value = Convert.ToInt32(cbCidade.SelectedValue.ToString());
+
+            try
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception error)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(error.Message,
+                        "Erro ao abrir conexão com o Banco de Dados",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception error)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(error.Message,
+                        "Erro ao tentar executar o comando SQL",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch { }
+            finally
+            {
+                conn.Close();
+
+                if (bIsOperationOK == true)
+                {
+                    MessageBox.Show("Registro Alterado!",
+                        "UPDATE",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+
+            AtualizaListaDeClientes();
+            btLimpar_Click(sender, e);
+
+        }
+
+        private void btExcluir_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            bool bIsOperationOK = true;
+
+            string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
+
+            conn = new SqlConnection(connectionString);
+
+            comm = new SqlCommand(
+                "UPDATE Clientes SET Nome=@Nome, CPF=@CPF, ID_Cidade=@ID_Cidade " +
+                "WHERE ID_Cliente = @ID_Cliente", conn);
+
+            comm.Parameters.Add("@ID_Cliente", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID_Cliente"].Value = Convert.ToInt32(txID.Text);
+
+            comm.Parameters.Add("@Nome", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@Nome"].Value = txNome.Text;
+
+            comm.Parameters.Add("@CPF", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@CPF"].Value = mtxCPF.Text;
+
+            comm.Parameters.Add("@ID_Cidade", System.Data.SqlDbType.Int);
+            comm.Parameters["@ID_Cidade"].Value = Convert.ToInt32(cbCidade.SelectedValue.ToString());
+
+            try
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception error)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(error.Message,
+                        "Erro ao abrir conexão com o Banco de Dados",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception error)
+                {
+                    bIsOperationOK = false;
+                    MessageBox.Show(error.Message,
+                        "Erro ao tentar executar o comando SQL",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch { }
+            finally
+            {
+                conn.Close();
+
+                if (bIsOperationOK == true)
+                {
+                    MessageBox.Show("Registro Alterado!",
+                        "UPDATE",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+
+            AtualizaListaDeClientes();
+            btLimpar_Click(sender, e);
+
+        }
+
+       
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            txID.Clear();
+            txNome.Clear();
+            mtxCPF.Clear();
+            cbCidade.Text = "";
+            txUF.Clear();
+            txID.Focus();
+        }
+
+ 
     }
 }
