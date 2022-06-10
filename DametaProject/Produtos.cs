2da17d5c3c9148c0d1ee862fffd8554970d63292
MatheusFinatto <matheusfinatto2@gmail.com>
2022-06-10 20:58:05 -0300

    10-06 products select

--- DametaProject/Produtos.cs
+++ DametaProject/Produtos.cs
@@ -30,12 +30,13 @@ namespace DametaProject
             conn = new SqlConnection(connectionString);
 
             comm = new SqlCommand(
-            "SELECT pro.cod_produto, pro.produtoNome, pro.preco, pro.estoque_id, pro.fornecedores_id, pro.tipo_produtos_id, est.id, est.qtd, forn.id, forn.fornecedoresNome, Tipo.id, Tipo.tipoNome " +
+            "SELECT pro.cod_produto, pro.nome as produtoNome, pro.preco, pro.estoque_id, pro.fornecedores_id, pro.tipo_produtos_id, est.id, " +
+            "est.qtd, forn.id, forn.nome as fornecedoresNome, Tipo.id, Tipo.nome as tipoNome " +
             "FROM produtos AS pro " +
                 "INNER JOIN estoque AS Est  ON Est.id = pro.estoque_id " +
                 "INNER JOIN fornecedores AS Forn  ON Forn.id = pro.fornecedores_id " +
                 "INNER JOIN tipo_produtos AS Tipo  ON Tipo.id = pro.tipo_produtos_id " +
-                "WHERE pro.cod_produto = 2", conn);
+                "WHERE pro.cod_produto = @ID", conn);
 
             comm.Parameters.Add("@ID", System.Data.SqlDbType.Int);
             comm.Parameters["@ID"].Value = Convert.ToInt32(txID.Text);
@@ -63,10 +64,13 @@ namespace DametaProject
                     // Se encontrou um cliente...
                     if (reader.Read())
                     {
+                        string preco = reader["preco"].ToString();
+                        preco = preco.Remove(preco.Length - 2);
+                        Console.WriteLine(preco);
                         txNome.Text = reader["produtoNome"].ToString();
-                        txPrecoUnit.Text = reader["preco"].ToString();
+                        txPrecoUnit.Text = preco;
                         txQtdEstoque.Text = reader["qtd"].ToString();
-                        cbFornecedor.Text = reader["fornecedorNome"].ToString();
+                        cbFornecedor.Text = reader["fornecedoresNome"].ToString();
                         cbTipo.Text = reader["tipoNome"].ToString();
                     }
 
@@ -97,85 +101,85 @@ namespace DametaProject
             txID.Focus();
         }
 
-        private void btIncluir_Click(object sender, EventArgs e)
-        {
-            SqlConnection conn;
-            SqlCommand comm;
-            bool bIsOperationOK = true;
+        //private void btIncluir_Click(object sender, EventArgs e)
+        //{
+        //    SqlConnection conn;
+        //    SqlCommand comm;
+        //    bool bIsOperationOK = true;
 
-            string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
+        //    string connectionString = Properties.Settings.Default.dameta_dbConnectionString;
 
-            conn = new SqlConnection(connectionString);
+        //    conn = new SqlConnection(connectionString);
 
-            comm = new SqlCommand(
-                "INSERT INTO produtos (produtoNome, preco, tipo_produtos_id, fornecedores_id, qtd) " +
-                "VALUES (@produtoNome, @preco, @tipo_produtos_id, @fornecedores_id, @qtd)", conn);
+        //    comm = new SqlCommand(
+        //        "INSERT INTO produtos (produtoNome, preco, tipo_produtos_id, fornecedores_id, qtd) " +
+        //        "VALUES (@produtoNome, @preco, @tipo_produtos_id, @fornecedores_id, @qtd)", conn);
 
-            comm.Parameters.Add("@produtoNome", System.Data.SqlDbType.NVarChar);
-            comm.Parameters["@produtoNome"].Value = txNome.Text;
+        //    comm.Parameters.Add("@produtoNome", System.Data.SqlDbType.NVarChar);
+        //    comm.Parameters["@produtoNome"].Value = txNome.Text;
 
-            comm.Parameters.Add("@preco", System.Data.SqlDbType.Date);
-            comm.Parameters["@preco"].Value = txPrecoUnit.Text;
+        //    comm.Parameters.Add("@preco", System.Data.SqlDbType.Date);
+        //    comm.Parameters["@preco"].Value = txPrecoUnit.Text;
 
-            comm.Parameters.Add("@tipo_produtos_id", System.Data.SqlDbType.Int);
-            comm.Parameters["@tipo_produtos_id"].Value = Convert.ToInt32(cbTipo.SelectedValue);
+        //    comm.Parameters.Add("@tipo_produtos_id", System.Data.SqlDbType.Int);
+        //    comm.Parameters["@tipo_produtos_id"].Value = Convert.ToInt32(cbTipo.SelectedValue);
 
-            comm.Parameters.Add("@fornecedores_id", System.Data.SqlDbType.Int);
-            comm.Parameters["@fornecedores_id"].Value = Convert.ToInt32(cbFornecedor.SelectedValue);
+        //    comm.Parameters.Add("@fornecedores_id", System.Data.SqlDbType.Int);
+        //    comm.Parameters["@fornecedores_id"].Value = Convert.ToInt32(cbFornecedor.SelectedValue);
 
-            comm.Parameters.Add("@estoque_id", System.Data.SqlDbType.Int);
-            comm.Parameters["@estoque_id"].Value = Convert.ToInt32(cbFornecedor.SelectedValue);
-
-
+        //    comm.Parameters.Add("@estoque_id", System.Data.SqlDbType.Int);
+        //    comm.Parameters["@estoque_id"].Value = Convert.ToInt32(cbFornecedor.SelectedValue);
 
 
-            try
-            {
-                try
-                {
-                    // Abre a conexão com o Banco de Dados
-                    conn.Open();
-                }
-                catch (Exception ex)
-                {
-                    bIsOperationOK = false;
-                    MessageBox.Show(ex.Message,
-                        "Erro ao tentar abrir o Banco de Dados",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
 
-                try
-                {
-                    comm.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    bIsOperationOK = false;
-                    MessageBox.Show(ex.Message,
-                        "Erro ao tentar executar o comando SQL.",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
-            }
-            catch { }
-            finally
-            {
-                // Fecha a conexão com o Bando de Dados
-                conn.Close();
 
-                if (bIsOperationOK == true)
-                {
-                    MessageBox.Show("Cliente Cadastrado com sucesso!",
-                        "INSERT",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Information);
+        //    try
+        //    {
+        //        try
+        //        {
+        //            // Abre a conexão com o Banco de Dados
+        //            conn.Open();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            bIsOperationOK = false;
+        //            MessageBox.Show(ex.Message,
+        //                "Erro ao tentar abrir o Banco de Dados",
+        //                MessageBoxButtons.OK,
+        //                MessageBoxIcon.Error);
+        //        }
 
-                    AtualizaListaDeClientes();
-                    btLimpar_Click(sender, e);
-                }
-            }
-        }
+        //        try
+        //        {
+        //            comm.ExecuteNonQuery();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            bIsOperationOK = false;
+        //            MessageBox.Show(ex.Message,
+        //                "Erro ao tentar executar o comando SQL.",
+        //                MessageBoxButtons.OK,
+        //                MessageBoxIcon.Error);
+        //        }
+        //    }
+        //    catch { }
+        //    finally
+        //    {
+        //        // Fecha a conexão com o Bando de Dados
+        //        conn.Close();
+
+        //        if (bIsOperationOK == true)
+        //        {
+        //            MessageBox.Show("Cliente Cadastrado com sucesso!",
+        //                "INSERT",
+        //                MessageBoxButtons.OK,
+        //                MessageBoxIcon.Information);
+
+        //            AtualizaListaDeClientes();
+        //            btLimpar_Click(sender, e);
+        //        }
+        //    }
+        //}
 
         //        private void btAlterar_Click(object sender, EventArgs e)
         //        {
@@ -321,11 +325,11 @@ namespace DametaProject
         //            btLimpar_Click(sender, e);
         //        }
 
-        //        private void Produtos_Load(object sender, EventArgs e)
-        //        {
-        //            // TODO: This line of code loads data into the 'dameta_dbDataSet.produtos' table. You can move, or remove it, as needed.
-        //            this.produtosTableAdapter.Fill(this.dameta_dbDataSet.produtos);
+        private void Produtos_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dameta_dbDataSet.produtos' table. You can move, or remove it, as needed.
+            this.produtosTableAdapter.Fill(this.dameta_dbDataSet.produtos);
 
-        //        }
+        }
     }
 }
