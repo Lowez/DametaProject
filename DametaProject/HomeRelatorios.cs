dce39efc06b9263872d8ef2755365d3bde58c961
MatheusFinatto <matheusfinatto2@gmail.com>
2022-06-24 15:26:02 -0300

    relatorios uncommented

--- DametaProject/HomeRelatorios.cs
+++ DametaProject/HomeRelatorios.cs
@@ -24,44 +24,44 @@ namespace DametaProject
 
         private void btControleEstoque_Click(object sender, EventArgs e)
         {
-            //Estoque form = new Estoque();
-            //form.Show();
+            Estoque form = new Estoque();
+            form.Show();
         }
 
         private void btFornecedores_Click(object sender, EventArgs e)
         {
-            //RelatorioFornecedores form = new RelatorioFornecedores();
-            //form.Show();
+            RelatorioFornecedores form = new RelatorioFornecedores();
+            form.Show();
         }
 
         private void btForncEstabelecimentos_Click(object sender, EventArgs e)
         {
-            //RelatoriosFornecedoresEstabelecimentos form = new RelatoriosFornecedoresEstabelecimentos();
-           // form.Show();
+            RelatoriosFornecedoresEstabelecimentos form = new RelatoriosFornecedoresEstabelecimentos();
+            form.Show();
         }
 
         private void btFuncEstabelecimento_Click(object sender, EventArgs e)
         {
-            //RelatorioFuncionarios form = new RelatorioFuncionarios();
-           // form.Show();
+            RelatorioFuncionarios form = new RelatorioFuncionarios();
+            form.Show();
         }
 
         private void btProdutos_Click(object sender, EventArgs e)
         {
-            //TopProdutos form = new TopProdutos();
-            //form.Show();
+            TopProdutos form = new TopProdutos();
+            form.Show();
         }
 
         private void btFaturamento_Click(object sender, EventArgs e)
         {
-            //Faturamento form = new Faturamento();
-            //form.Show();
+            Faturamento form = new Faturamento();
+            form.Show();
         }
 
         private void btTabelaPrecos_Click(object sender, EventArgs e)
         {
-            //TabelaDePrecos form = new TabelaDePrecos();
-            //form.Show();
+            TabelaDePrecos form = new TabelaDePrecos();
+            form.Show();
         }
     }
 }
